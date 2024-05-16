d4afe530f08604a92327425b31a6f662d9c30de4
Patrick de Matos <65696357+patrickdmatos@users.noreply.github.com>
2024-05-16 17:58:51 -0300

    consumindo rota monstros

--- src/pages/Bestiario/bestiario.tsx
+++ src/pages/Bestiario/bestiario.tsx
@@ -1,48 +1,82 @@
-import { Card, Flex, Heading, Text } from "@chakra-ui/react";
-import { ObtemMonstros } from "./hooks/ObtemMonstros";
+import { Accordion, AccordionButton, AccordionItem, AccordionPanel, Box, Card, Flex, Grid, Heading, Text } from "@chakra-ui/react";
+import { ObtemArrayMonstros, InfosMonstro } from "./hooks/ObtemMonstros";
+import { ReactElement } from "react";
 
 interface IMonstros {
-    count: number
-    results: IArrayMosntros[]
-}
-
-interface IArrayMosntros {
+    map(arg0: (monstro: IMonstros) => import("react/jsx-runtime").JSX.Element): unknown;
     name: string;
     index: string;
     url: string;
 }
 
-const Bestiario = () => {
-    const { data, error, isLoading } = ObtemMonstros();
+
+
+const Bestiario = (): JSX.Element => {
+    const { data, error, isLoading } = ObtemArrayMonstros();
 
     const renderMonstros = (monstros: IMonstros) => {
-        console.log(monstros.results[0]);
 
         return (
-            <Flex flexDir={'column'} textAlign="center" alignItems={'center'} py="20" bgColor={'gray.300'} h={'100vh'} color={'yellow.600'}>
-                <Heading as="h1" size="xl" mb="6">Bem-vindo à Taberna da Cadela Prenha!</Heading>
-                <Text fontSize="lg" fontWeight={600}>Essa é a base da Guilda do Doguinho Caramelo.</Text>
-                <Text fontSize="lg" fontWeight={600} mb="8">Aqui você pode registrar suas aventuras e conseguir informações valiosas.</Text>
-                {
-                    monstros.results.map((monstro: IArrayMosntros, index: number) => (
-                        <>
-                            <Card w={'30%'} maxH={'30px'} key={index}>
-                                <Text >{monstro.name}</Text>
-                            </Card>
-                        </>
-                    ))
+            monstros.map((monstro: IMonstros): ReactElement<JSX.Element> => {
+                const { data: monstrosInfos } = InfosMonstro(monstro.url);
+
+                if (!monstrosInfos) {
+                    console.log('Monstro não encontrado');
                 }
-            </Flex>
+
+                const tamanhosDesejados = ['Huge', 'Large', 'Medium'];
+
+                return (
+                    <Accordion allowToggle key={monstro.index}>
+                        {tamanhosDesejados.map((tamanho: string) => (
+                            <AccordionItem key={tamanho}>
+                                {({ isExpanded }) => (
+                                    <>
+                                        <h2>
+                                            <AccordionButton>
+                                                <Box as='span' flex='1' textAlign='left'>
+                                                    <Heading fontSize={16}>
+                                                        {tamanho}
+                                                    </Heading>
+                                                </Box>
+                                                {isExpanded ? 'MENOS' : 'MAIS'}
+                                            </AccordionButton>
+                                        </h2>
+                                        <AccordionPanel pb={4}>
+                                            {monstrosInfos && monstrosInfos?.size.includes(tamanho) ? (
+                                                <Card mb={4} p={4} bgColor={'gray.700'} color={'yellow.400'}>
+                                                    <Heading as="h2" size="lg" mb={4}>{monstrosInfos.name}</Heading>
+                                                    <Text fontSize="md" mb={2}>Tamanho: {monstrosInfos.size}</Text>
+                                                    <Text fontSize="md" mb={2}>Tipo: {monstrosInfos.type}</Text>
+                                                    <Text fontSize="md" mb={2}>Alineamento: {monstrosInfos.alignment}</Text>
+                                                </Card>
+                                            ) : (
+                                                'Não há monstros nesse tamanho.'
+                                            )}
+                                        </AccordionPanel>
+                                    </>
+                                )}
+                            </AccordionItem>
+                        ))}
+                    </Accordion>
+                );
+            })
+
         );
     };
 
     return (
-        <div>
-            <h1>Bestiário</h1>
-            {isLoading && <p>Carregando...</p>}
-            {error && <p>Ocorreu um erro ao carregar os monstros</p>}
-            {data && renderMonstros(data as IMonstros)}
-        </div>
+        <>
+            <Flex flexDir={'column'} textAlign="center" alignItems={'center'} py={8} bgColor={'gray.500'} h={'100vh'} color={'yellow.400'}>
+                <Heading as="h1" size="xl" mb="6">Bem-vindo à Taberna da Cadela Prenha!</Heading>
+                <Text fontSize="lg" fontWeight={600}>Essa é a base da Guilda do Doguinho Caramelo.</Text>
+                <Text fontSize="lg" fontWeight={600} mb="8">Aqui você pode registrar suas aventuras e conseguir informações valiosas.</Text>
+
+                {isLoading && <p>Carregando...</p>}
+                {error && <p>Ocorreu um erro ao carregar os monstros</p>}
+                {data && renderMonstros(data as IMonstros)}
+            </Flex >
+        </>
     );
 };
 
--- src/pages/Bestiario/hooks/ObtemMonstros.ts
+++ src/pages/Bestiario/hooks/ObtemMonstros.ts
@@ -9,9 +9,125 @@ interface IMonstros {
     results: []
 }
 
-export const ObtemMonstros = () => {
+
+interface IArmorClass {
+    type: string;
+    value: number;
+}
+
+interface ISpeed {
+    walk: string;
+    swim: string;
+}
+
+interface IProficiency {
+    value: number;
+    proficiency: {
+        index: string;
+        name: string;
+        url: string;
+    };
+}
+
+interface IDamageType {
+    index: string;
+    name: string;
+    url: string;
+}
+
+interface IDamage {
+    damage_type: IDamageType;
+    damage_dice: string;
+}
+
+interface IAction {
+    name: string;
+    multiattack_type?: string;
+    desc: string;
+    actions?: Array<{
+        action_name: string;
+        count: number;
+        type: string;
+    }>;
+    attack_bonus?: number;
+    dc?: {
+        dc_type: {
+            index: string;
+            name: string;
+            url: string;
+        };
+        dc_value: number;
+        success_type: string;
+    };
+    damage?: IDamage[];
+}
+
+interface ILegendaryAction {
+    name: string;
+    desc: string;
+    attack_bonus?: number;
+    damage?: IDamage[];
+}
+
+interface IMonstrosInfos {
+    index: string;
+    name: string;
+    size: string;
+    type: string;
+    alignment: string;
+    armor_class: IArmorClass[];
+    hit_points: number;
+    hit_dice: string;
+    hit_points_roll: string;
+    speed: ISpeed;
+    strength: number;
+    dexterity: number;
+    constitution: number;
+    intelligence: number;
+    wisdom: number;
+    charisma: number;
+    proficiencies: IProficiency[];
+    damage_vulnerabilities: string[];
+    damage_resistances: string[];
+    damage_immunities: string[];
+    condition_immunities: string[];
+    senses: {
+        darkvision: string;
+        passive_perception: number;
+    };
+    languages: string;
+    challenge_rating: number;
+    proficiency_bonus: number;
+    xp: number;
+    special_abilities: Array<{
+        name: string;
+        desc: string;
+        dc?: {
+            dc_type: {
+                index: string;
+                name: string;
+                url: string;
+            };
+            dc_value: number;
+            success_type: string;
+        };
+    }>;
+    actions: IAction[];
+    legendary_actions: ILegendaryAction[];
+    image: string;
+    url: string;
+}
+
+export const ObtemArrayMonstros = () => {
     return useQuery<IMonstros[], Error>('monstros', async () => {
         const response = await axios.get(`${urlBase}/api/monsters`);
-        return response.data;
+        return response.data.results;
     });
 };
+
+export const InfosMonstro = (monstroURL: string) => {
+    return useQuery<IMonstrosInfos, Error>(['monstro', monstroURL], async () => {
+        const response = await axios.get(`${urlBase}${monstroURL}`);
+        return response.data;
+    });
+}
